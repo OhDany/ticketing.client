c22764cdec79f7bcd13dca4d0ecddcdf39e12aaa
Oh Dany <ohdany@Oh-Dany.local>
2020-10-26 18:55:29 -0600

    Add Header in all Pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
 const Named = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />;
+    </div>
+  );
 };
 
 export default Named;
